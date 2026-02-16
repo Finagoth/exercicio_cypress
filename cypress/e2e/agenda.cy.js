4ae4c88643c8c6b3a5be30dd1631dd9136597622
Lucas Calíope <lucascaliope2010@email.com>
2026-02-16 09:12:35 -0300

    feat: adicionando testes com Cypress

--- cypress/e2e/agenda.cy.js
+++ cypress/e2e/agenda.cy.js
@@ -1,5 +1,17 @@
-describe('Teste Agenda EBAC', () => {
-  it('Deve abrir o site', () => {
+describe('Testes da Agenda de Contatos', () => {
+
+  beforeEach(() => {
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
+
+  it('Deve adicionar um novo contato', () => {
+    cy.get('input[placeholder="Nome"]').type('Lucas Teste')
+    cy.get('input[placeholder="E-mail"]').type('lucas@teste.com')
+    cy.get('input[placeholder="Telefone"]').type('11993451499')
+
+    cy.contains('button', 'Adicionar').click()
+
+    cy.contains('Lucas Teste').should('exist')
+  })
+
 })
