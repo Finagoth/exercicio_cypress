2bd894f2efd51489fdd94fab5e8ca082c9928f46
Lucas Calíope <lucascaliope2010@gmail.com>
2026-02-17 16:05:56 -0300

    Update agenda.cy.js

--- cypress/e2e/agenda.cy.js
+++ cypress/e2e/agenda.cy.js
@@ -1,17 +1,35 @@
-describe('Testes da Agenda de Contatos', () => {
+describe('Testes Agenda de Contatos', () => {
 
   beforeEach(() => {
     cy.visit('https://ebac-agenda-contatos-tan.vercel.app/')
   })
 
-  it('Deve adicionar um novo contato', () => {
-    cy.get('input[placeholder="Nome"]').type('Lucas Teste')
+  it('Deve incluir um novo contato', () => {
+    cy.get('input[placeholder="Nome"]').type('Maria Silva')
+    cy.get('input[placeholder="E-mail"]').type('maria@email.com')
+    cy.get('input[placeholder="Telefone"]').type('11999999999')
+    cy.contains('Adicionar').click()
+
+    cy.contains('Maria Silva').should('exist')
+  })
+
+  it('Deve alterar um contato', () => {
+    cy.get('input[placeholder="Nome"]').type('Lucas Silva')
     cy.get('input[placeholder="E-mail"]').type('lucas@teste.com')
-    cy.get('input[placeholder="Telefone"]').type('11993451499')
+    cy.get('input[placeholder="Telefone"]').type('11988888823')
+    cy.contains('Adicionar').click()
 
-    cy.contains('button', 'Adicionar').click()
+    cy.contains('Editar').first().click()
 
-    cy.contains('Lucas Teste').should('exist')
+    cy.get('input[placeholder="Nome"]').clear().type('Lucas Alterado')
+    cy.contains('Salvar').click()
+
+    cy.contains('Lucas Alterado').should('exist')
+  })
+
+  it('Deve remover um contato', () => {
+    
+    cy.contains('Deletar').first().click()
   })
 
 })
